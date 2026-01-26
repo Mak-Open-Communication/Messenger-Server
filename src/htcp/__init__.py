92e0f18abc27a66cf8ae9ac101ba5ea4ab2cdc86
mk-samoilov <me@mk-samoilov.ru>
2026-01-26 18:12:27 +0300

    lib bugfix

--- src/htcp/__init__.py
+++ src/htcp/__init__.py
@@ -5,10 +5,7 @@ A library for building TCP-based client-server applications
 with automatic type serialization and RPC support.
 """
 
-from .server import Server
-from .client import Client
 from .aio_server import AsyncServer
-from .aio_client import AsyncClient
 from .exceptions import (
     HTCPError,
     ConnectionError,
@@ -23,12 +20,8 @@ from .exceptions import (
 
 __version__ = "0.2.0"
 __all__ = [
-    # Sync
-    'Server',
-    'Client',
     # Async
     'AsyncServer',
-    'AsyncClient',
     # Exceptions
     'HTCPError',
     'ConnectionError',
