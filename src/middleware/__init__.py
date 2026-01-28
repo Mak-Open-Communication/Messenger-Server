852b055f48e7d2aa44abec6901c306642d869486
mk-samoilov <me@mk-samoilov.ru>
2026-01-28 07:59:56 +0300

    update logging middleware

---
+++ src/middleware/__init__.py
@@ -0,0 +1,4 @@
+from src.middleware.logging_middleware import LoggingMiddleware as LoggingMiddlewareInstance
+
+
+LoggingMiddleware = LoggingMiddlewareInstance()
--- src/middleware/logging_middleware.py
+++ src/middleware/logging_middleware.py
@@ -1,7 +1,10 @@
 import sys
 import logging
+import time
 
+from functools import wraps
 from pathlib import Path
+from typing import Callable
 
 from src.config import settings
 
@@ -44,3 +47,72 @@ def setup_logging() -> None:
         file_handler.setLevel(log_level)
         file_handler.setFormatter(formatter)
         root_logger.addHandler(file_handler)
+
+
+class LoggingMiddleware:
+    """Logging transactions/subscriptions middleware."""
+
+    def __init__(self):
+        self.logger = logging.getLogger("request-logger")
+        self.logger.setLevel(settings.logging_level)
+
+    @staticmethod
+    def _sanitize_params(params: dict) -> dict:
+        """
+        Removes sensitive data from parameters before logging
+        """
+        sensitive_keys = {"password", "password_hash", "token", "secret", "api_key", "private_key"}
+
+        sanitized = {}
+        for key, value in params.items():
+            if any(sensitive in key.lower() for sensitive in sensitive_keys):
+                sanitized[key] = "***REDACTED***"
+            else:
+                sanitized[key] = value
+
+        return sanitized
+
+    def log_transaction(self, func: Callable) -> Callable:
+        """
+                Decorator for logging transactions (handlers).
+                It logs the input parameters, execution time, and the result/error.
+
+                Usage:
+                    logger_middleware = LoggingMiddleware()
+
+                    @server.transaction(code="send_message")
+                    @logger_middleware.log_transaction
+                    def send_message(...):
+                        pass
+                """
+
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            transaction_name = func.__name__
+            start_time = time.time()
+
+            # safe_kwargs = self._sanitize_params(kwargs.copy())
+
+            try:
+                result = func(*args, **kwargs)
+                duration = time.time() - start_time
+
+                self.logger.info(
+                    f"Transaction '{transaction_name}' from success handled [within {duration:.3f}s]"
+                )
+
+                return result
+
+            except Exception as e:
+                duration = time.time() - start_time
+
+                self.logger.error(
+                    f"Transaction failed: {transaction_name} | "
+                    f"Error: {type(e).__name__}: {str(e)} | "
+                    f"Duration: {duration:.3f}s",
+                    exc_info=True
+                )
+
+                raise
+
+        return wrapper
