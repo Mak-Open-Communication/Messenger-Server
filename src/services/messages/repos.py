050be86235a8da7bf989cfba0571e08a05dbf947
mk-samoilov <me@mk-samoilov.ru>
2026-02-03 13:35:16 +0300

    services structure reform

--- src/services/messages/repos.py
+++ src/services/messages/repos.py
@@ -1,10 +1,60 @@
-"""Messages, msg contents and tags repositories"""
+"""Messages, msg contents, tags & uploaded files repositories"""
+
+import mimetypes
 
 from typing import Optional, List
 
-from src.common.base_repos import BaseDBRepository
+from src.common.base_repos import BaseDBRepository, BaseS3Repository
 
 from src.models.db_models import MessageDB, MessageContentDB, MessageTagDB
+
+
+class UsersFilesRepository(BaseS3Repository):
+    """Uploaded files repository (on messages)"""
+
+    repository_name = "users-msg-files"
+    resources_dir = "users/uploaded_files/"
+
+    async def upload(
+        self,
+        chat_id: int,
+        message_id: int,
+        filename: str,
+        file_bytes: bytes
+    ) -> tuple[str, str]:
+        """Upload file to S3 and return resource_name and URL"""
+
+        resource_name = f"{chat_id}/{message_id}/{filename}"
+        full_key = self._get_full_key(resource_name)
+
+        # Guess content type
+        content_type, _ = mimetypes.guess_type(filename)
+
+        # Upload to S3
+        await self.s3.upload_file(full_key, file_bytes, content_type=content_type)
+
+        # Generate presigned URL
+        url = await self.s3.get_file_url(full_key, expires_in=3600)
+
+        return resource_name, url
+
+    async def download(self, resource_name: str) -> Optional[bytes]:
+        """Download file from S3 by resource_name"""
+
+        full_key = self._get_full_key(resource_name)
+        return await self.s3.download_file(full_key)
+
+    async def delete(self, resource_name: str) -> bool:
+        """Delete file from S3"""
+
+        full_key = self._get_full_key(resource_name)
+        return await self.s3.delete_file(full_key)
+
+    async def exists(self, resource_name: str) -> bool:
+        """Check if file exists in S3"""
+
+        full_key = self._get_full_key(resource_name)
+        return await self.s3.file_exists(full_key)
 
 
 class MessagesRepository(BaseDBRepository):
--- src/services/messages/service.py
+++ src/services/messages/service.py
@@ -5,9 +5,12 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from src.app import Application
 
-from src.services.messages.repos import MessagesRepository, MessageContentsRepository, MessageTagsRepository
-
-from src.services.media_repos import UsersFilesRepository
+from src.services.messages.repos import (
+    MessagesRepository,
+    MessageContentsRepository,
+    MessageTagsRepository,
+    UsersFilesRepository
+)
 
 from src.models.api_models import Message
 
@@ -19,8 +22,13 @@ class MessageService:
         self.app = app
 
         self.messages_repo = MessagesRepository(app)
-        self.uploaded_files_repo = UsersFilesRepository(app)
         self.contents_repo = MessageContentsRepository(app)
         self.tags_repo = MessageTagsRepository(app)
 
+        self.uploaded_files_repo = UsersFilesRepository(app)
+
+    def create_message(self) -> Message:
+        pass
+
     def get_message_by_id(self, message_id: int) -> Message:
+        pass
